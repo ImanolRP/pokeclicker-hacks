2d4291cfaf6a49bb4d800e7be1739f5eef96f284
ImanolRP <imanol_ramirez@hotmail.com>
2022-07-10 02:29:16 +0200

    optimiced algorithm for dungeons

--- src/automations/dungeon.js
+++ src/automations/dungeon.js
@@ -1,24 +1,40 @@
-export function autoDungeon() {
-    if (App.game.gameState === GameConstants.GameState.dungeon) {
-        const tempDungeonGrid = DungeonRunner.map.board()
-        for (let x = 0; x < DungeonRunner.map.size; x++) {
-            for (let y = 0; y < DungeonRunner.map.size; y++) {
-                if (DungeonRunner.map.currentTile().type() === GameConstants.DungeonTile.chest) {
-                    DungeonRunner.openChest();
-                } else if (DungeonRunner.map.currentTile().type() === GameConstants.DungeonTile.boss && !DungeonRunner.fightingBoss()) {
-                    DungeonRunner.startBossFight();
-                } else {
-                    DungeonRunner.map.moveToCoordinates(x, y)
-                }
-                if (tempDungeonGrid !== DungeonRunner.map.board()) {
-                    break
-                }
-            }
-            if (tempDungeonGrid !== DungeonRunner.map.board()) {
-                break
+// global variable
+let bossTyle
+
+// help function
+function findBossTyle() {
+    for (let x = 0; x < DungeonRunner.map.size; x++) {
+        for (let y = 0; y < DungeonRunner.map.size; y++) {
+            if (DungeonRunner.map.board()[y][x].type() == GameConstants.DungeonTile.boss) {
+                bossTyle = new Point(x, y)
+                console.log(bossTyle)
             }
         }
+    }
+}
+
+// automator
+export function autoDungeon() {
+    if (!player.town().dungeon) {
+        // Secure return for not bug the game if dungeon dosent exist
+        return
+    }
+    if (App.game.gameState === GameConstants.GameState.dungeon) {
+        // actions on dungeon
+        if (bossTyle.y < DungeonRunner.map.playerPosition().y) {
+            DungeonRunner.map.moveUp()
+        } else if (bossTyle.x < DungeonRunner.map.playerPosition().x) {
+            DungeonRunner.map.moveLeft()
+        } else if (bossTyle.x > DungeonRunner.map.playerPosition().x) {
+            DungeonRunner.map.moveRight()
+        } else {
+            DungeonRunner.startBossFight()
+        }
+        // default action for open chest on the way 
+        DungeonRunner.openChest()
     } else if (App.game.gameState === GameConstants.GameState.town) {
+        // reset dungeon when finished
         DungeonRunner.initializeDungeon(player.town().dungeon)
+        findBossTyle()
     }
 }
