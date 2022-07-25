28dd96e3a84f5402ea3f64d35197b416abe06840
ImanolRP <imanol_ramirez@hotmail.com>
2022-07-25 23:39:40 +0200

    test auto-load when script tag added

--- src/index.js
+++ src/index.js
@@ -8,14 +8,18 @@ import { autoShopper } from '~/automations/shopper'
 import { autoGymnast } from '~/automations/gymnast'
 import { autoCoach } from '~/automations/coach'
 
-const middleColumn = document.getElementById('middle-column');
-middleColumn.insertBefore(scriptsPanel, middleColumn.firstChild);
+let middleColumn;
 
-appendToggleButton('Hatcher', autoHatcher);
-appendToggleButton('Farmer', autoFarmer);
-appendToggleButton('Miner', autoMiner);
-appendToggleButton('Clicker', autoClicker);
-appendToggleButton('Shopper', autoShopper);
-appendToggleButton('Coach', autoCoach);
-appendToggleButton('Gymnast', autoGymnast);
-appendToggleButton('Dungeon', autoDungeon);
+(function () {
+    middleColumn = document.getElementById('middle-column');
+    middleColumn.insertBefore(scriptsPanel, middleColumn.firstChild);
+
+    appendToggleButton('Hatcher', autoHatcher);
+    appendToggleButton('Farmer', autoFarmer);
+    appendToggleButton('Miner', autoMiner);
+    appendToggleButton('Clicker', autoClicker);
+    appendToggleButton('Shopper', autoShopper);
+    appendToggleButton('Coach', autoCoach);
+    appendToggleButton('Gymnast', autoGymnast);
+    appendToggleButton('Dungeon', autoDungeon);
+})();
